2bebe0821674e24d90a93c40f6063a84d8124e92
unknown <pete.carey@yahoo.co.uk>
2021-01-06 19:23:48 +0000

    Observable HeroService

---
+++ src/app/hero.service.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
+  }
+  constructor() {}
+}
